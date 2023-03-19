ae28f277da91884dc2e5008cb829a9a716dd9c2f
asucanyh-cn <asucanyh@outlook.com>
2023-03-19 12:23:30 +0800

    modified:   recovery.sh

--- recovery.sh
+++ recovery.sh
@@ -8,22 +8,22 @@
 ####
 blog="myblog"                                                             #博客数据包解压后的文件夹名
 currentPath=$PWD                                                          #博客所在目录
-dataFile='*.zip'                                                          #可定义数据文件名
+dataFile='blog-source.zip'                                                #可定义数据文件名
 nodeurl="https://nodejs.org/dist/v16.16.0/node-v16.16.0-linux-x64.tar.xz" # nodejs版本链接（可自定义）
 ####用于git配置
 email=asucanyh-cn@outlook.com
 username=asucanyh-cn
+remoteRepo=blog-source
 ####
-if [ "$#" -eq 2 ]; then
-  if [ "${#2}" -lt 3 ]; then
-    echo -e "[Error]Please check your parameter for dataFile!\n"
-    exit 17
+function checkParams() {
+  if [ "$#" -eq 2 ]; then
+    if [ "${#2}" -lt 3 ]; then
+      echo -e "[Error]Please check your parameter for dataFile!\n"
+      exit 17
+    fi
+    dataFile=$2
   fi
-  dataFile=$2
-else
-  echo -e "[Note]Please specify the datafile."
-fi
-
+}
 function restart() {
   echo -e "\n[Info]Cleaning nodejs & $blog..."
   nohup rm -rf $currentPath/nodejs &>/dev/null
@@ -63,10 +63,9 @@ function checkOS() {
   nohup sudo $pkgm update -y --skip-broken &>/dev/null
 }
 function env() {
-  ####
   ## 安装git
   nohup $pkgm install -y git &>/dev/null
-  if [ "$?" == 0 ]; then
+  if [ "$?" -eq 0 ]; then
     echo -e "[Info]'git' installed successfully!"
   else
     echo -e "\n[Error]Please check 'git'\n"
@@ -75,7 +74,7 @@ function env() {
 
   ## 安装wget
   nohup $pkgm install -y wget &>/dev/null
-  if [ "$?" == 0 ]; then
+  if [ "$?" -eq 0 ]; then
     echo -e "[Info]'wget' installed successfully!"
   else
     echo -e "\n[Error]Please check 'wget'\n"
@@ -89,10 +88,14 @@ function env() {
     mv $(find $currentPath -name node-v[0-9]*) $currentPath/nodejs
   fi
   # 重新创建nodejs软链接
+  ###
+  rm -rf /usr/local/nodejs
+  mv $currentPath/nodejs /usr/local
+  ###
   rm -f /usr/local/bin/node
-  ln -s $currentPath/nodejs/bin/node /usr/local/bin/node
+  ln -s /usr/local/nodejs/bin/node /usr/local/bin/node
   rm -f /usr/local/bin/npm
-  ln -s $currentPath/nodejs/bin/npm /usr/local/bin/npm
+  ln -s /usr/local/nodejs/bin/npm /usr/local/bin/npm
   if [ "$?" -eq 0 ]; then
     nohup node -v &>/dev/null
   fi
@@ -115,18 +118,21 @@ function env() {
   ## 安装Hexo
   nohup hexo &>/dev/null
   if [ "$?" -ne 0 ]; then
-    echo ""
     nohup npm install --location=global hexo-cli &>/dev/null
   fi
   ### 链接hexo命令至环境变量中
   rm -f /usr/local/bin/hexo
-  ln -s $currentPath/nodejs/lib/node_modules/hexo-cli/bin/hexo /usr/local/bin/hexo
+  ln -s /usr/local/nodejs/lib/node_modules/hexo-cli/bin/hexo /usr/local/bin/hexo
 }
 function unzip() {
+  if [ "$#" -eq 1 ]; then
+    echo -e "[Note]Please specify the datafile."
+    exit 25
+  fi
   ## 还原数据
   ### 解压数据包 并修改文件夹为$blog
   if [ ! -d "$currentPath/$blog" ]; then
-    if [ ${dataFile:0-3} == '.gz' ]; then
+    if [ "${dataFile:0-3}" == ".gz" ]; then
       tar -xf $currentPath/$dataFile
       if [ "$?" -eq 0 ]; then
         mv $currentPath/${dataFile%*.tar.gz} $currentPath/$blog
@@ -143,35 +149,48 @@ function unzip() {
     echo -e "[Info]Now,you can switch to '$blog' and use 'hexo server' to preview your site!\n[Note]Done"
   else
     echo -e "\n[Error]Please check 'tar -xf $dataFile or 'unzip $dataFile.'\n"
-    # exit 80
+    exit 80
   fi
 }
-function gitConfig(){
+function gitConfig() {
   git config --global user.email "$email"
   git config --global user.name "$username"
   git config --global credential.helper store
   git config --global http.sslVerify false
   git config --global init.defaultBranch main
 }
-function repairNPM(){
-  cd $myblog
-  rm -rf node_modules && npm install --force
-  npm audit fix --force
+function repairNPM() {
+  cd $blog
+  echo -e "[Info]Repairing the errors."
+  rm -rf node_modules && nohup npm install --force &>/dev/null
+  rm -rf .deploy_git/
+  # git config --global core.autocrlf false
+  cd ../
+  echo -e "[Info]Errors repaired."
+}
+function initGit() {
+  cd $blog
+  git init
+  git add .
+  git remote add origin https://github.com/$username/$remoteRepo.git
+  git remote update
+  git checkout main
   cd ../
 }
+
+##main
+checkParams
 case $1 in
 "start")
   checkOS
   env
   gitConfig
-  repairNPM
   unzip
   ;;
 "env")
   checkOS
   env
   gitConfig
-  repairNPM
   ;;
 "restart")
   checkOS
@@ -180,15 +199,27 @@ case $1 in
   restart
   unzip
   ;;
+"unzip")
+  unzip
+  ;;
+"repair")
+  repairNPM
+  ;;
+"init")
+  initGit
+  ;;
 "clean")
   clean
   ;;
 "help")
-  echo -e "[Info]env|clean|start|restart\n"
-  echo -e "env:Only install the necessary utils.\n"
-  echo -e "clean:Only clean the *.tar.gz files.\n"
-  echo -e "start:Start recovery nomarlly."
-  echo -e "restart:Reinstall nodejs and re-unzip your blog.\n"
+  echo -e "Usage: <Command> [OPTION] [FILE]"
+  echo -e "    env:     Only install the necessary utils."
+  echo -e "    repair:  Repair npm module errors."
+  echo -e "    init:    Link the remote reposity with local reposity."
+  echo -e "    unzip:   Only unpack the datafile."
+  echo -e "    clean:   Clean the necessary files."
+  echo -e "    start:   Start recovery nomarlly."
+  echo -e "    restart: Reinstall nodejs and re-unzip your blog.\n"
   ;;
 *)
   echo "[!] invalid input"
